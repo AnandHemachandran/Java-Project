d6fbe67ffd61936a5e5f93ae341a04456280b357
AnandHemachandran <anand.h1729@gmail.com>
2019-10-16 14:12:23 +0530

    Rebuild

--- Helping Hands/src/Form/Main.java
+++ Helping Hands/src/Form/Main.java
@@ -1,7 +1,6 @@
 package Form;
 import Form.UI.MainPanel;
 import Form.UI.RegisterationFrom;
-
 public class Main {
     public static void main(String[] args) {
         MainPanel p = new MainPanel("Helping Hands");
